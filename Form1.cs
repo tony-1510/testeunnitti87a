be1b914af1def4f61dbf267d20813e1e85b84dcb
anthony.spinheiro <pinheiro-170698@hotmail.com>
2020-11-09 10:50:09 -0300

    Inserir um botão no form

--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,10 @@ namespace testeunnitti87a
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Gitou kchorrera!");
+        }
     }
 }
